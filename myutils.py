7eb4c56d18b0fb6f85d000af8c44149da74e6ffb
sephyr84 <sephyr84@gmail.com>
2020-07-28 13:57:23 +0900

    Update myutils.py

--- myutils.py
+++ myutils.py
@@ -1,13 +1,25 @@
 from functools import wraps
 import time
 
+class color:
+  PURPLE = '\033[95m'
+  CYAN = '\033[96m'
+  DARKCYAN = '\033[36m'
+  BLUE = '\033[94m'
+  GREEN = '\033[92m'
+  YELLOW = '\033[93m'
+  RED = '\033[91m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+  END = '\033[0m'
+ 
 def my_timer(original_function):
   @wraps(original_function)
   def wrapper(*args, **kwargs):
     t1 = time.time()
     result = original_function(*args, **kwargs)
     t2 = time.time() - t1
-    print('[ {} ] function execution time: {:10.4f} sec'.format(original_function.__name__, t2))
+    print('[' + color.CYAN + original_function.__name__ + color.END + '] function execution time: {:10.4f} sec'.format(t2))
     return result
   return wrapper
 
